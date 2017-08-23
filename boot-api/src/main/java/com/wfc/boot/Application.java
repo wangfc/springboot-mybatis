f3cd82f0210e731c9c67091e01f9e8eeb94db85a
wangfuchu <wangfuchu@fingercrm.cn>
2017-08-23 11:33:27 +0800

    git 测试

--- boot-api/src/main/java/com/wfc/boot/Application.java
+++ boot-api/src/main/java/com/wfc/boot/Application.java
@@ -14,6 +14,7 @@ public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
+        System.out.println(1122);
     }
 
     @Bean
